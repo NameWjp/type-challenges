ca9098ae1e2bbbdd61f6ee0c9f41d9db04480679
wangjp <943375372@qq.com>
2023-05-10 23:29:02 +0800

    update

--- easy/43-Exclude/test-cases.ts
+++ easy/43-Exclude/test-cases.ts
@@ -1,6 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
-// 巧妙的利用 never 来排除 K 类型的子集
+// extends 可以以两种方式使用，一个是真正的 extends，另一个就像一个具有 T 遍历的三元表达式，称为分配条件类型
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
 type MyExclude<T, K> = T extends K ? never : T;
 
 type cases = [
